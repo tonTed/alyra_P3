ad6e0e84eb301b012f1ec773ad207944dc99e36c
Teddy Blanco <tblanco@student.42quebec.com>
2022-04-17 09:24:03 -0400

    :wrench: truffle-config

--- truffle-config.js
+++ truffle-config.js
@@ -5,8 +5,23 @@ module.exports = {
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
-    develop: {
-      port: 8545
-    }
-  }
+		development: {
+			host: "127.0.0.1",     // Localhost (default: none)
+			port: 8545,            // Standard Ethereum port (default: none)
+			network_id: "*",       // Any network (default: none)
+		},
+	},
+	mocha: {
+		reporter: 'eth-gas-reporter',
+		reporterOptions: {
+			gasPrice: 1,
+			token: 'ETH',
+		}
+	},
+	plugins: ["solidity-coverage"],
+	compilers: {
+		solc: {
+			version: "0.8.13",    	// Fetch exact version from solc-bin (default: truffle's version)
+		}
+	},
 };
