fb45d0d856fa46bc68ed3aaa3d1264731f7b6c54
Teddy Blanco <tblanco@student.42quebec.com>
2022-04-20 12:31:29 -0400

    Updates

---
+++ precache-manifest.84f9b611c3c7926957726ec8aa4652d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c419abd6b3a1f7deff1f6b301f9c79a8",
+    "url": "/alyra_P3/index.html"
+  },
+  {
+    "revision": "4617c6e7a948ca14c8f0",
+    "url": "/alyra_P3/static/css/main.6976293d.chunk.css"
+  },
+  {
+    "revision": "f2ab181472efde173a2b",
+    "url": "/alyra_P3/static/js/2.e3afd581.chunk.js"
+  },
+  {
+    "revision": "4617c6e7a948ca14c8f0",
+    "url": "/alyra_P3/static/js/main.3a3677e1.chunk.js"
+  },
+  {
+    "revision": "bac81e2ec6820d9e5fa3",
+    "url": "/alyra_P3/static/js/runtime-main.cfe9ebfc.js"
+  }
+]);
